339000faf93bf035ddc75847d58efdd0d02f1137
jduckerOWP <jason.ducker@noaa.gov>
2026-08-31 15:13:03 +0000

    Revamp Puerto Rico forcing extraction scripts to define each operational forcing product as a Python class to build the target URLs and download the target URLS based on AWS and NOMADs repositories as available. Include a new Python class that streamlines NBM AND NAM Nest AnA cycling.

--- NextGen_Forcings_Engine_BMI/Forcing_Extraction_Scripts/Puerto_Rico/get_prod_NBM_Puerto_Rico_AnA.py
+++ NextGen_Forcings_Engine_BMI/Forcing_Extraction_Scripts/Puerto_Rico/get_prod_NBM_Puerto_Rico_AnA.py
@@ -3,7 +3,7 @@ import os
 from Forcing_Extraction_Scripts.forecast_download_base import ForecastDownloader
 
 
-class NBMPuertoRicoDownloader(ForecastDownloader):
+class NBMAnAPuertoRicoDownloader(ForecastDownloader):
     """
     Downloader for NBM forecast data over Puerto Rico using predictable filenames.
 
@@ -14,43 +14,42 @@ class NBMPuertoRicoDownloader(ForecastDownloader):
 
     @property
     def base_url(self):
+        """Sets the base url for NBM Puerto Rico AnA data."""
         return "https://noaa-nbm-grib2-pds.s3.amazonaws.com"
 
-    def should_process_hour(self, d_start):
-        return d_start.hour in [0, 6, 12, 18]
-
-    def get_download_targets(self, d_start):
-        hourly = range(1, 37)  # 1 through 36
-        every_3h = range(36, 193, 3)  # 123 through 240, step of 3
-        every_6h = range(198, 265, 6)  # 198 through 264, step of 6
-        return list(hourly) + list(every_3h) + list(every_6h) if d_start.hour in [0, 6, 12, 18] else []
-        # return range(1, 265) if d_start.hour in [0, 6, 12, 18] else []
+    def get_download_targets(self, _):
+        """Sets the forecast hours to download."""
+        return range(0, 2)
 
     def build_output_dir(self, d_start, _):
+        """Creates the output directory path."""
         return os.path.join(
             self.out_dir,
             f"blend.{d_start.strftime('%Y%m%d')}",
-            d_start.strftime('%H'),
-            "core"
+            d_start.strftime("%H"),
+            "core",
         )
 
     def build_file_url_and_name(self, d_start, target, _):
+        """Constructs the download URL and filename for a given forecast hour."""
         fhr_str = f"f{str(target).zfill(3)}"
         filename = f"blend.t{d_start.strftime('%H')}z.core.{fhr_str}.pr.grib2"
         url = os.path.join(
             self.base_url,
             f"blend.{d_start.strftime('%Y%m%d')}",
-            d_start.strftime('%H'),
+            d_start.strftime("%H"),
             "core",
-            filename
+            filename,
         )
+
         return url, filename
 
     @property
     def recursive_cleanup(self) -> bool:
+        """Indicates whether cleanup should be recursive."""
         return True
 
 
 if __name__ == "__main__":
-    downloader = NBMPuertoRicoDownloader.from_cli_args()
+    downloader = NBMAnAPuertoRicoDownloader.from_cli_args()
     downloader.run()
